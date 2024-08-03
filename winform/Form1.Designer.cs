ab139abbed96992e01c640e018bdae94f5d1c51f
bekaboob <165276365+d0lmany@users.noreply.github.com>
2024-08-03 17:40:34 +0500

    Add files via upload

---
+++ winform/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.outUser = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.outPost = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.outComment = new System.Windows.Forms.ListBox();
+            this.start = new System.Windows.Forms.Button();
+            this.outPhoto = new System.Windows.Forms.PictureBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.outPhotoList = new System.Windows.Forms.ListBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.left = new System.Windows.Forms.Button();
+            this.right = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.outPhoto)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.outUser);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(269, 223);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "юзеры";
+            // 
+            // outUser
+            // 
+            this.outUser.FormattingEnabled = true;
+            this.outUser.ItemHeight = 16;
+            this.outUser.Location = new System.Drawing.Point(6, 21);
+            this.outUser.Name = "outUser";
+            this.outUser.Size = new System.Drawing.Size(257, 196);
+            this.outUser.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.outPost);
+            this.groupBox2.Location = new System.Drawing.Point(287, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(269, 223);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "посты";
+            // 
+            // outPost
+            // 
+            this.outPost.FormattingEnabled = true;
+            this.outPost.ItemHeight = 16;
+            this.outPost.Location = new System.Drawing.Point(6, 21);
+            this.outPost.Name = "outPost";
+            this.outPost.Size = new System.Drawing.Size(257, 196);
+            this.outPost.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.outComment);
+            this.groupBox3.Location = new System.Drawing.Point(562, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(269, 223);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "коменты";
+            // 
+            // outComment
+            // 
+            this.outComment.FormattingEnabled = true;
+            this.outComment.ItemHeight = 16;
+            this.outComment.Location = new System.Drawing.Point(6, 21);
+            this.outComment.Name = "outComment";
+            this.outComment.Size = new System.Drawing.Size(257, 196);
+            this.outComment.TabIndex = 0;
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.start.Location = new System.Drawing.Point(562, 241);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(269, 111);
+            this.start.TabIndex = 3;
+            this.start.Text = "fetch!";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // outPhoto
+            // 
+            this.outPhoto.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.outPhoto.Location = new System.Drawing.Point(6, 21);
+            this.outPhoto.Name = "outPhoto";
+            this.outPhoto.Size = new System.Drawing.Size(257, 196);
+            this.outPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.outPhoto.TabIndex = 4;
+            this.outPhoto.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.outPhotoList);
+            this.groupBox4.Location = new System.Drawing.Point(12, 241);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(269, 223);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "фотки";
+            // 
+            // outPhotoList
+            // 
+            this.outPhotoList.FormattingEnabled = true;
+            this.outPhotoList.ItemHeight = 16;
+            this.outPhotoList.Location = new System.Drawing.Point(6, 21);
+            this.outPhotoList.Name = "outPhotoList";
+            this.outPhotoList.Size = new System.Drawing.Size(257, 196);
+            this.outPhotoList.TabIndex = 0;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.outPhoto);
+            this.groupBox5.Location = new System.Drawing.Point(287, 241);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(269, 223);
+            this.groupBox5.TabIndex = 6;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "фото";
+            // 
+            // left
+            // 
+            this.left.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.left.Location = new System.Drawing.Point(562, 358);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(134, 106);
+            this.left.TabIndex = 7;
+            this.left.Text = "<";
+            this.left.UseVisualStyleBackColor = true;
+            this.left.Click += new System.EventHandler(this.left_Click);
+            // 
+            // right
+            // 
+            this.right.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.right.Location = new System.Drawing.Point(697, 358);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(134, 106);
+            this.right.TabIndex = 8;
+            this.right.Text = ">";
+            this.right.UseVisualStyleBackColor = true;
+            this.right.Click += new System.EventHandler(this.right_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 479);
+            this.Controls.Add(this.right);
+            this.Controls.Add(this.left);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.outPhoto)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.ListBox outPost;
+        private System.Windows.Forms.ListBox outComment;
+        private System.Windows.Forms.ListBox outUser;
+        private System.Windows.Forms.PictureBox outPhoto;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ListBox outPhotoList;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Button left;
+        private System.Windows.Forms.Button right;
+    }
+}
+
---
+++ winform/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Drawing;
+using System.Net.NetworkInformation;
+
+namespace winform
+{
+    public partial class Form1 : Form
+    {
+        //ссылки на источники
+        const string urlUser = "https://jsonplaceholder.typicode.com/users";
+        const string urlPost = "https://jsonplaceholder.typicode.com/posts";
+        const string urlComment = "https://jsonplaceholder.typicode.com/comments";
+        const string urlPhoto = "https://jsonplaceholder.typicode.com/photos";
+        static string[] album;
+        static int currentphoto = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void start_Click(object sender, EventArgs e)
+        {
+            if (VerifyNet())
+            {
+                //извлекаем данные
+                await FetchData(urlUser, outUser,"User");
+                await FetchData(urlPost, outPost, "Post");
+                await FetchData(urlComment, outComment, "Comment");
+                await FetchData(urlPhoto, outPhotoList, "Photo");
+                CreateAlbum(outPhoto,outPhotoList);
+            }
+            else { MessageBox.Show("Подключение к сети отсутствует"); }
+        }
+        //метод для проверки подключения
+        static bool VerifyNet()
+        {
+            try
+            {
+                Ping ping = new Ping();
+                PingReply reply = ping.Send("yandex.ru");
+                return reply.Status == IPStatus.Success;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        static async Task FetchData(string url, ListBox listBox,string current)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    //собственно, fetch
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    //сохраняем jsonы
+                    List<User> jsons = JsonConvert.DeserializeObject<List<User>>(responseBody);
+                    //получаем содержимое
+                    string[] outs = {};
+                    switch (current)
+                    {
+                        case "User":
+                        outs = GetSaveStrings(jsons);
+                        break;
+                        case "Post":
+                        outs = GetSaveStringsOne(jsons);
+                        break;
+                        case "Comment":
+                        outs = GetSaveStringsOne(jsons);
+                        break;
+                        case "Photo":
+                        outs = GetSaveStringsTwo(jsons);
+                        break;
+                    }
+                    //выводим в окно
+                    listBox.Items.AddRange(outs);
+                }
+                catch
+                {
+                    MessageBox.Show("отрежь себе руки");
+                }
+            }
+        }
+        //методы для преобразования
+        static public string[] GetSaveStrings(List<User> give)
+        {
+            List<string> get = new List<string>();
+            foreach (var str in give)
+            {
+                get.Add(str.Name);
+            }
+            return get.ToArray();
+        }
+        static public string[] GetSaveStringsOne(List<User> give)
+        {
+            List<string> get = new List<string>();
+            foreach (var str in give)
+            {
+                get.Add(str.Body);
+            }
+            return get.ToArray();
+        }
+        static public string[] GetSaveStringsTwo(List<User> give)
+        {
+            List<string> get = new List<string>();
+            foreach (var str in give)
+            {
+                get.Add(str.Url);
+            }
+            return get.ToArray();
+        }
+        //МЕТОДЫ ДЛЯ КАРТИНОЧЕК
+            //создание альбома
+        static void CreateAlbum(PictureBox box,ListBox from)
+        {
+            //получаем список ссылкок на фото
+            if (from.Items.Count > 0)
+            {
+            List<string> temp = new List<string>();
+            foreach (var item in from.Items)
+            {
+                temp.Add(item.ToString());
+            }
+            album = temp.ToArray();
+            //Выводим первое изображение
+            OutPhotos(album[currentphoto],ref box);
+            }
+            else
+            {
+                MessageBox.Show("Нет ссылок");
+            }
+            
+        }
+        //загрузка + вывод через веб-клиента
+        static void OutPhotos(string url,ref PictureBox box)
+        {
+            try
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    //Загрузка данных по ссылке
+                    byte[] data = webClient.DownloadData(url);
+
+                    //Создание фото из массива байтов
+                    using (MemoryStream ms = new MemoryStream(data))
+                    {
+                        Image image = Image.FromStream(ms);
+
+                        //Вывод
+                        box.Image = image;
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось загрузить фото");
+            }
+        }
+        //след. фото
+        private void right_Click(object sender, EventArgs e)
+        {
+            if(outPhotoList.Items.Count != currentphoto)
+            {
+                currentphoto++;
+                OutPhotos(album[currentphoto], ref outPhoto);
+            }
+            else { MessageBox.Show("Это последнее изображение"); }
+        }
+        //пред. фото
+        private void left_Click(object sender, EventArgs e)
+        {
+            if (currentphoto != 0)
+            {
+                currentphoto--;
+                OutPhotos(album[currentphoto], ref outPhoto);
+            }
+            else { MessageBox.Show("Это первое изображение"); }
+        }
+    }
+    //класс для json
+    public class User
+    {
+        public string Name { get; set; }
+        public string Body { get; set; }
+        public string Url { get; set; }
+    }
+}
